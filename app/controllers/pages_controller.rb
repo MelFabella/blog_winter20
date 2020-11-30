5113f0870d3bd347a218b07fe029ecd66a41b85a
Mel Fabella <mf820am@gmail.com>
2020-11-30 07:20:22 -0800

    added controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,47 @@
 class PagesController < ApplicationController
+  # GET all page 
   def index
+    @pages = Page.all
+    # render index.html.erb
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
-
+  
+  def create 
+    @page = Page.new(pages_params)
+    if @page.save 
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+  
   def edit
+    @page = Page.find(params[:index])
   end
+  
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit 
+    end
+  end
+  
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+  
+  private
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 end
